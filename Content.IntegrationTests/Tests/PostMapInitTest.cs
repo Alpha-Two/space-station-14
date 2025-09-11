173d91c9a14d51d9e950530c1339038da50dbb0c
UpAndLeaves <92269094+Alpha-Two@users.noreply.github.com>
2025-09-11 07:32:43 +0100

    Revert "Add admin shuttles (#32139)"
    This reverts commit 2882fd289f502b9f0b394ee067766191bdc3bf0c.

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -73,7 +73,6 @@ namespace Content.IntegrationTests.Tests
         private static readonly string[] DoNotMapWhitelist =
         {
             "/Maps/centcomm.yml",
-            "/Maps/Shuttles/AdminSpawn/**" // admin gaming
         };
 
         /// <summary>
